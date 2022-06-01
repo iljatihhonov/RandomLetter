9c3a3f90d24bb4a8ecf131027a7c2ba516ac3635
ilja.tihhonov <ilja.tihhonov@outlook.com>
2022-06-01 21:29:28 +0300

    А нужны ли коммиты?

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RandomLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вводим имя
+            Console.Write("Hi, enter your name: ");
+            string name = Console.ReadLine();
+            //Дёргаем рандомную букву из введённого имени
+            Console.WriteLine("Random letter from your name: {0}", name?[new Random().Next(name.Length)]);
+        }
+    }
+}
